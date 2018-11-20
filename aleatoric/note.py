8555c6c7bcbfdb360154bd1db6e0f24d3c698d70
marksweiss <marksimonweiss@gmail.com>
2018-11-20 00:12:06 -0500

    Integration test calls SC with not errors. No sound output.

--- aleatoric/note.py
+++ aleatoric/note.py
@@ -21,8 +21,11 @@ class NoteAttrs(object):
 
     @staticmethod
     def __validate(instrument, start, dur, amp, pitch):
-        if not instrument or not isinstance(start, float) or not isinstance(dur, float) or \
-                not isinstance(amp, float) or not isnum(pitch, float):
+        if not instrument or \
+                (not isinstance(start, float) and not isinstance(start, int)) or \
+                not isinstance(dur, float) or \
+                not isinstance(amp, float) or \
+                (not isinstance(pitch, int) and not isinstance(pitch, float)):
             raise ValueError((f'Must provide value for required NoteAttrs args: '
                               f'instrument: {instrument} start {start} dur: {dur} amp: {amp} pitch: {pitch}'))
 
@@ -40,12 +43,11 @@ class SupercolliderNoteAttrs(NoteAttrs):
               'romanianMinor', 'yu', 'zhi'}
 
     # noinspection PyShadowingBuiltins
-    def __init__(self, synth_def: Any = None, dur: float = None,
-                 amp: float = None, degree: float = None, name: str = None,
-                 oct: int = None, scale: str = None):
+    def __init__(self, synth_def: Any = None, delay: int = None,
+                 dur: float = None, amp: float = None, degree: float = None,
+                 name: str = None, oct: int = None, scale: str = None):
         # start not meaningful for Supercollider
-        start = 0.0
-        super(SupercolliderNoteAttrs, self).__init__(instrument=synth_def, start=start, dur=dur,
+        super(SupercolliderNoteAttrs, self).__init__(instrument=synth_def, start=delay, dur=dur,
                                                      amp=amp, pitch=degree, name=name)
         if oct:
             if not isinstance(oct, int):
@@ -55,6 +57,24 @@ class SupercolliderNoteAttrs(NoteAttrs):
             if scale not in SupercolliderNoteAttrs.SCALES:
                 raise ValueError(f'scale: {scale} must must be in allowed scales {SupercolliderNoteAttrs.SCALES}')
             self.scale = scale
+
+    @property
+    def delay(self):
+        return self.start
+
+    @delay.setter
+    def delay(self, delay: int):
+        # noinspection PyAttributeOutsideInit
+        self.start = delay
+
+    @property
+    def degree(self):
+        return self.pitch
+
+    @degree.setter
+    def degree(self, degree: float):
+        # noinspection PyAttributeOutsideInit
+        self.pitch = degree
 
     def __str__(self):
         s = (f'name: {self.name} synth_def: {self.instrument} start: {self.start} '
@@ -66,120 +86,49 @@ class SupercolliderNoteAttrs(NoteAttrs):
         return s
 
 
-class PerformanceAttrsFrozenException(Exception):
-    pass
-
-
-class PerformanceAttrs(object):
-    """Open container for dynamically adding performance attributes to a Note. These attributes
-       govern the behavior of the Instrument playing the Note, and not attributes of the Note itself.
-       So for example they might parameterize `vibrato` or `tremolo` in a SupercolliderNote.
-
-       Different instruments on different platforms expose different parameters, so this class has no
-       fixed attributes. So instead it is just an API for adding attributes in a type-safe way.
-       The API lets the caller add an attribute by name and type, and dynamically adds the attribute
-       and a validator for it.
-
-       Usage:
-       - call add_attr(name, type) for each attribute you want to add
-       - call freeze() when you want to make the object read-only
-    """
-    attr_type_map: Dict[str, Any]
-
-    DEFAULT_NAME = 'PERF_ATTRS'
-
-    def __init__(self, name: str = None):
-        self.name = name or PerformanceAttrs.DEFAULT_NAME
-        self.frozen: bool = False
-        self.attr_type_map = {}
-        self.config = PerformanceAttrs()
-
-    def add_attr(self, attr_name: str = None, val=None, attr_type=None):
-        if not isinstance(attr_name, str) or not attr_type:
-            raise ValueError((f'type: {attr_type} must be a type '
-                              f'and name: {name} must be a string'))
-        if self.frozen:
-            raise PerformanceAttrsFrozenException((f'Attempt to set attribute: {attr_name} '
-                                                   f'on frozen PerformanceConfigFactory: {self.name}'))
-        self.attr_type_map[attr_name] = type
-        setattr(self, attr_name, val)
-
-    def __setattr__(self, attr_name, val):
-        if attr_name not in self.attr_type_map:
-            raise ValueError('Invalid attribute name')
-        if not isinstance(val, self.attr_type_map[attr_name]):
-            raise ValueError(f'val: {val} must be of type: {self.attr_type_map[attr_name]}')
-        super(PerformanceAttrs, self).__setattr__(attr_name, val)
-
-    def freeze(self):
-        self.frozen = True
-
-    def unfreeze(self):
-        self.frozen = False
-
-    def is_frozen(self):
-        return self.frozen
-
-    def __str__(self):
-        return ' '.join([f'{attr_name}: {getattr(self, attr_name)}' for attr_name in self.attr_type_map.keys()])
-
-    def asdict(self):
-        return {attr_name: getattr(self, attr_name) for attr_name in self.attr_type_map.keys()}
-
-
-class Note(object):
-    def __init__(self, note_attrs: NoteAttrs = None, performance_attrs: PerformanceAttrs = None):
-        # performance_attrs is optional
-        if not isinstance(note_attrs, NoteAttrs) or \
-                performance_attrs and not isinstance(performance_attrs, PerformanceAttrs):
-            raise ValueError((f'Must provide valid `note_attrs` and `performance_attrs`: '
-                              f'note_attrs: {note_attrs} performance_attrs: {performance_attrs}'))
-        self.note_attrs = note_attrs
-        self.performance_attrs = performance_attrs
-
-
-class RestNote(Note):
-    """Models the core attributes of a musical note common to multiple back ends
-       with amplitude set to 0
-    """
-
-    REST_AMP = 0.0
-
-    def __init__(self, note_attrs: NoteAttrs = None, performance_attrs: PerformanceAttrs = None):
-        super(RestNote, self).__init__(note_attrs=note_attrs, performance_attrs=performance_attrs)
-        self.amp = RestNote.REST_AMP
-
-    @staticmethod
-    def to_rest(note: Note):
-        note.amp = RestNote.REST_AMP
-
-
-class SupercolliderNote(Note):
-    """Models a note with attributes aliased to and specific to Supercollider
-    """
-
-    def __init__(self, note_attrs: SupercolliderNoteAttrs = None, performance_attrs: PerformanceAttrs = None):
-        super(SupercolliderNote, self).__init__(note_attrs=note_attrs, performance_attrs=performance_attrs)
-
-    @property
-    def degree(self):
-        return self.pitch
-
-    @degree.setter
-    def degree(self, degree: float):
-        # noinspection PyAttributeOutsideInit
-        self.pitch = degree
-
-
-class CSoundNote(Note):
+class CSoundNoteAttrs(NoteAttrs):
     """Models a note with attributes aliased to and specific to CSound
        and with a str() that prints CSound formatted output.
     """
+    def __init__(self, instrument: Any = None, start: float = None,
+                 duration: float = None, amplitude: float = None,
+                 pitch: float = None, name: str = None):
+        super(CSoundNoteAttrs, self).__init__(instrument=instrument, start=start, dur=duration,
+                                                     amp=amplitude, pitch=pitch, name=name)
 
-    def __init__(self, note_attrs: NoteAttrs = None, performance_attrs: PerformanceAttrs = None):
-        if not performance_attrs or not hasattr(performance_attrs, 'instrument'):
-            raise ValueError(f'performance_attrs: {performance_attrs} must provide values for arg `instrument`')
-        super(CSoundNote, self).__init__(note_attrs=note_attrs, performance_attrs=performance_attrs)
+    @property
+    def duration(self):
+        return self.dur
+
+    @duration.setter
+    def duration(self, duration: float):
+        # noinspection PyAttributeOutsideInit
+        self.dur = duration
+
+    @property
+    def amplitude(self):
+        return self.amp
+
+    @amplitude.setter
+    def amplitude(self, amplitude: float):
+        # noinspection PyAttributeOutsideInit
+        self.amp = amplitude
+
+
+class MidiNoteAttrs(NoteAttrs):
+    """Models a note with attributes aliased to and specific to MIDI
+       and with a str() that prints MIDI formatted output.
+    """
+
+    DEFAULT_CHANNEL = 1
+
+    def __init__(self, instrument: Any = None, time: float = None,
+                 duration: float = None, velocity: float = None,
+                 pitch: float = None, name: str = None):
+        if not hasattr(performance_attrs, 'channel'):
+            performance_attrs.channel = MidiNote.DEFAULT_CHANNEL
+        super(MidiNoteAttrs, self).__init__(instrument=instrument, start=time, dur=duration,
+                                            amp=velocity, pitch=pitch, name=name)
 
     @property
     def time(self):
@@ -208,30 +157,98 @@ class CSoundNote(Note):
         # noinspection PyAttributeOutsideInit
         self.amp = velocity
 
-
-class MidiNote(Note):
-    """Models a note with attributes aliased to and specific to MIDI
-       and with a str() that prints MIDI formatted output.
-    """
-
-    DEFAULT_CHANNEL = 1
-
-    def __init__(self, note_attrs: NoteAttrs = None, performance_attrs: PerformanceAttrs = None):
-        if not hasattr(performance_attrs, 'channel'):
-            performance_attrs.channel = MidiNote.DEFAULT_CHANNEL
-        super(MidiNote, self).__init__(note_attrs=note_attrs, performance_attrs=performance_attrs)
-
-    @property
-    def velocity(self):
-        return self.amp
-
-    @velocity.setter
-    def velocity(self, velocity: float):
-        # noinspection PyAttributeOutsideInit
-        self.amp = velocity
-
     def program_change(self, instrument: int):
         if instrument is None:
             raise ValueError(f'MidiNote must provide value for required arg instrument: {instrument}')
         # noinspection PyAttributeOutsideInit
         self.instrument = instrument
+
+
+class PerformanceAttrsFrozenException(Exception):
+    pass
+
+
+class PerformanceAttrs(object):
+    """Open container for dynamically adding performance attributes to a Note. These attributes
+       govern the behavior of the Instrument playing the Note, and not attributes of the Note itself.
+       So for example they might parameterize `vibrato` or `tremolo` in a SupercolliderNote.
+
+       Different instruments on different platforms expose different parameters, so this class has no
+       fixed attributes. So instead it is just an API for adding attributes in a type-safe way.
+       The API lets the caller add an attribute by name and type, and dynamically adds the attribute
+       and a validator for it.
+
+       Usage:
+       - call add_attr(name, type) for each attribute you want to add
+       - call freeze() when you want to make the object read-only
+    """
+    DEFAULT_NAME = 'PERF_ATTRS'
+
+    def __init__(self, name: str = None):
+        self.attr_type_map: Dict[str, Any] = {}
+        self.name: str = name or PerformanceAttrs.DEFAULT_NAME
+        self.frozen: bool = False
+
+    def add_attr(self, attr_name: str = None, val=None, attr_type=None):
+        if not isinstance(attr_name, str) or not attr_type:
+            raise ValueError((f'type: {attr_type} must be a type '
+                              f'and name: {name} must be a string'))
+        if self.frozen:
+            raise PerformanceAttrsFrozenException((f'Attempt to set attribute: {attr_name} '
+                                                   f'on frozen PerformanceConfigFactory: {self.name}'))
+        self.attr_type_map[attr_name] = type
+
+        print(val)
+
+        setattr(self, attr_name, val)
+
+    def safe_set_attr(self, attr_name, val):
+        if attr_name not in self.attr_type_map:
+            raise ValueError('Invalid attribute name')
+        if not isinstance(val, self.attr_type_map[attr_name]):
+            raise ValueError(f'val: {val} must be of type: {self.attr_type_map[attr_name]}')
+        setattr(self, attr_name, val)
+
+    def freeze(self):
+        self.frozen = True
+
+    def unfreeze(self):
+        self.frozen = False
+
+    def is_frozen(self):
+        return self.frozen
+
+    def __str__(self):
+        print(getattr(self, 'synth_def'))
+
+        return ' '.join([f'{attr_name}: {getattr(self, attr_name)}' for attr_name in self.attr_type_map.keys()])
+
+    def asdict(self):
+        return {attr_name: getattr(self, attr_name) for attr_name in self.attr_type_map.keys()}
+
+
+class Note(object):
+    def __init__(self, note_attrs: NoteAttrs = None, performance_attrs: PerformanceAttrs = None):
+        # performance_attrs is optional
+        if not isinstance(note_attrs, NoteAttrs) or \
+                performance_attrs and not isinstance(performance_attrs, PerformanceAttrs):
+            raise ValueError((f'Must provide valid `note_attrs` and `performance_attrs`: '
+                              f'note_attrs: {note_attrs} performance_attrs: {performance_attrs}'))
+        self.note_attrs = note_attrs
+        self.performance_attrs = performance_attrs
+
+
+class RestNote(Note):
+    """Models the core attributes of a musical note common to multiple back ends
+       with amplitude set to 0
+    """
+
+    REST_AMP = 0.0
+
+    def __init__(self, note_attrs: NoteAttrs = None, performance_attrs: PerformanceAttrs = None):
+        super(RestNote, self).__init__(note_attrs=note_attrs, performance_attrs=performance_attrs)
+        self.note_attrs.amp = RestNote.REST_AMP
+
+    @staticmethod
+    def to_rest(note: Note):
+        note.note_attrs.amp = RestNote.REST_AMP
--- aleatoric/player.py
+++ aleatoric/player.py
@@ -1,15 +1,16 @@
 # Copyright 2018 Mark S. Weiss
 
-from abc import abstractmethod, ABC
+from abc import abstractmethod, ABCMeta
 from typing import List
 
-from FoxDot import Player
+from FoxDot import Player as SC_Player
+# noinspection PyProtectedMember
 from FoxDot.lib.SCLang._SynthDefs import pluck as sc_sd_pluck
 
-from note import Note, PerformanceAttrs, SupercolliderNote, SupercolliderNoteAttrs
+from note import Note, PerformanceAttrs, SupercolliderNoteAttrs
 
 
-class Player(ABC):
+class Player(metaclass=ABCMeta):
 
     # TODO ADD play_all() and figure out "note group" with one perf_attr_config for all note_attrs
 
@@ -17,33 +18,36 @@ class Player(ABC):
     def play_each(self):
         raise NotImplementedError()
 
-    def __init__(self, notes: List[Note] = None):
+    def __init__(self, notes: List[Note]):
         # Only validate if the list is not empty
         if notes is not None and notes:
             # All elements are a subclass of Note. This still allows heterogeneous Notes.
             for note in notes:
                 if not isinstance(note, Note):
                     raise ValueError(f'arg notes must be None or empty list or type List[Note]')
-        self.notes = notes or list()
+        self.notes = notes
 
 
 class SupercolliderPlayer(Player):
-
-    def __init__(self, notes=None):
-        super(SupercolliderPlayer, self).__init__(notes=notes)
-        self.sc_player = Player()
+    def __init__(self, notes: List[Note]):
+        super(SupercolliderPlayer, self).__init__(notes)
+        self.sc_player = SC_Player()
 
     def play_each(self):
-        for note in self.notes:
-            self.sc_player >> note.note_attrs.instrument([note.note_attrs.degree],
-                                                         dur=note.note_attrs.dur, amp=note.note_attrs.amp,
-                                                         **note.performance_attrs.asdict())
+        for n in self.notes:
+            self.sc_player >> n.note_attrs.instrument([n.note_attrs.degree],
+                                                      dur=n.note_attrs.dur,
+                                                      amp=n.note_attrs.amp,
+                                                      **n.performance_attrs.asdict())
 
 
 if __name__ == '__main__':
-    note_attrs = SupercolliderNoteAttrs(synth_def=sc_sd_pluck(), dur=1.0, amp=0.5, degree=1,
-                                        name='test_note', oct=5, scale='chromatic')
+    note_attrs = SupercolliderNoteAttrs(synth_def=sc_sd_pluck,
+                                        delay=0, dur=1.0, amp=1.0, degree=1,
+                                        name='test_note',
+                                        oct=4, scale='chromatic')
     performance_attrs = PerformanceAttrs()
     note = Note(note_attrs=note_attrs, performance_attrs=performance_attrs)
-    player = SupercolliderPlayer(notes=[note])
+    notes = [note]
+    player = SupercolliderPlayer(notes=notes)
     player.play_each()
