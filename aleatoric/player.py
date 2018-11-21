b4c74df4cd1a75ca92878f6c0ccbdac3d04b29be
marksweiss <marksimonweiss@gmail.com>
2018-11-21 09:49:58 -0500

    Added pre-play hooks, post-play hooks, improvising to API of Player base class

--- aleatoric/player.py
+++ aleatoric/player.py
@@ -3,7 +3,7 @@
 from time import sleep
 
 from abc import abstractmethod, ABCMeta
-from typing import List
+from typing import Any, Dict, List
 
 from FoxDot import Player as SC_Player
 # noinspection PyProtectedMember
@@ -17,14 +17,13 @@ class PlayerNoNotesException(Exception):
 
 
 class Player(metaclass=ABCMeta):
-
-    # TODO ADD play_all() and figure out "note group" with one perf_attr_config for all note_attrs
-
     @abstractmethod
     def play_each(self):
         """Play each note. Each notes note_attrs are honored and
            separate performance_attrs with a matching index are applied
            to all notes in the batch if they are available.
+
+           Implementers can choose to use pre-play or post-play hooks as they wish.
         """
         raise NotImplementedError()
 
@@ -32,6 +31,16 @@ class Player(metaclass=ABCMeta):
     def play_all(self):
         """Play notes as a batch. Each notes note_attrs are honored but
            performance_attrs are applied to all notes in the batch.
+
+           Implementers can choose to use pre-play or post-play hooks as they wish.
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def improvise(self):
+        """Player generates notes according to the concrete class implementation and plays them.
+
+           Implementers should handle checking the state of self.improvising == False.
         """
         raise NotImplementedError()
 
@@ -47,6 +56,41 @@ class Player(metaclass=ABCMeta):
             if not isinstance(note_group, NoteGroup):
                 raise ValueError(f'arg `note_group` must be type NoteGroup')
             self.note_group = note_group
+
+        self.improvising = False
+
+        self.pre_play_hooks: Dict[str, Any] = {}
+        self.pre_play_hooks_list: List[Any] = []
+        self.post_play_hooks: Dict[str, int] = {}
+        self.post_play_hooks_list: List[Any] = []
+
+    def add_pre_play_hook(self, name: str, hook: Any):
+        self._add_hook(name, hook, self.pre_play_hooks, self.pre_play_hooks_list)
+
+    def add_post_play_hook(self, name: str, hook: Any):
+        self._add_hook(name, hook, self.post_play_hooks, self.post_play_hooks_list)
+
+    @staticmethod
+    def _add_hook(name, hook, hooks, hooks_list):
+        if not name or not isinstance(name, str) or not hook or '__call__' not in dir(hook):
+            raise ValueError(f'args `name` must be a non-empty string and `hook` must be a callable')
+        hooks[name] = hook
+        hooks_list.append(hook)
+
+    def remove_pre_play_hook(self, name: str):
+        self._remove_hook(name, self.pre_play_hooks, self.pre_play_hooks_list)
+
+    def remove_post_play_hook(self, name: str):
+        self._remove_hook(name, self.post_play_hooks, self.post_play_hooks_list)
+
+    @staticmethod
+    def _remove_hook(name, hooks, hooks_list):
+        if not name or not isinstance(name, str):
+            raise ValueError(f'arg `name` must be a non-empty string')
+        hook = hooks.get(name)
+        if hook:
+            hooks_list.remove(hook)
+            del hooks[name]
 
 
 class SupercolliderPlayer(Player):
@@ -75,6 +119,9 @@ class SupercolliderPlayer(Player):
                                                     **note_group.performance_attrs.as_dict())
             sleep(note_attrs.dur)
             self.sc_player.stop()
+
+    def improvise(self):
+        raise NotImplementedError('SupercolliderPlayer does not support improvising')
 
 
 if __name__ == '__main__':
