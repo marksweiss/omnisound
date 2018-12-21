0baf2388ea6197fb7e531a67beb79098694ade2e
marksweiss <marksimonweiss@gmail.com>
2018-12-21 02:40:00 -0500

    Test coverage for Scale. It's working

--- aleatoric/test/test_scale.py
+++ aleatoric/test/test_scale.py
@@ -13,7 +13,7 @@ INSTRUMENT = 1
 START = 0.0
 DUR = 1.0
 AMP = 100
-PITCH = 10.1
+PITCH = 9.1
 
 KEY = MajorKey.C
 OCTAVE = 4
@@ -21,14 +21,22 @@ SCALE_CLS = ScaleCls.Major
 NOTE_CLS = CSoundNote
 
 
-@pytest.fixture('module')
+@pytest.fixture
 def note():
     return CSoundNote(instrument=INSTRUMENT, start=START, duration=DUR, amplitude=AMP, pitch=PITCH)
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture
 def scale(note):
     return Scale(key=KEY, octave=OCTAVE, scale_cls=SCALE_CLS, note_cls=NOTE_CLS, note_prototype=note)
+
+
+def test_scale(note, scale):
+    assert scale.key == KEY
+    assert scale.octave == OCTAVE
+    assert scale.note_type is NOTE_CLS
+    assert scale.scale_type is SCALE_CLS
+    assert scale.note_prototype == note
 
 
 def test_is_major_key_is_minor_key(note, scale):
@@ -43,9 +51,22 @@ def test_is_major_key_is_minor_key(note, scale):
     assert scale_minor.is_minor_key
 
 
-# TODO TEST GET EXPECTED NOTES
-def test_get_pitch_for_key(scale):
-    pass
+def test_get_pitch_for_key(note, scale):
+    # Expect that Scale.__init__() will populate the underlying NoteSequence with the notes for the `scale_cls`
+    # and `key` (type of scale and root key), starting at the value of `octave` arg passed to Scale.__init__()
+    expected_pitches = (4.01, 4.03, 4.05, 4.06, 4.08, 4.10, 4.12, 5.01)
+    pitches = [n.pitch for n in scale.note_list]
+    for i, expected_pitch in enumerate(expected_pitches):
+        assert pytest.approx(expected_pitch, pitches[i])
+
+    # MinorKey case, HarmonicMinor class in Mingus
+    scale_minor = Scale(key=MinorKey.C, octave=OCTAVE, scale_cls=ScaleCls.HarmonicMinor, note_cls=NOTE_CLS,
+                        note_prototype=note)
+    expected_pitches = (4.01, 4.03, 4.04, 4.06, 4.08, 4.09, 4.12, 5.01)
+    pitches = [n.pitch for n in scale_minor.note_list]
+    for i, expected_pitch in enumerate(expected_pitches):
+        assert pytest.approx(expected_pitch, pitches[i])
+
 
 
 if __name__ == '__main__':
