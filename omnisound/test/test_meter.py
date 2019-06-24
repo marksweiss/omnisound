42c8d086d6f010c15d08634edcaa2188594ed61c
marksweiss <marksimonweiss@gmail.com>
2019-06-24 10:50:34 -0400

    Fixes to test_meter. test_note, test_measure (with skipped tests) and test_meter working with new Note backend.

--- omnisound/test/test_meter.py
+++ omnisound/test/test_meter.py
@@ -1,5 +1,6 @@
 # Copyright 2018 Mark S. Weiss
 
+from numpy import array, copy as np_copy
 import pytest
 
 from omnisound.note.adapters.csound_note import CSoundNote
@@ -11,6 +12,9 @@ START = 0.0
 DUR = float(NoteDur.QUARTER.value)
 AMP = 1.0
 PITCH = 10.1
+ATTRS = array([float(INSTRUMENT), START, DUR, AMP, PITCH])
+ATTR_NAME_IDX_MAP = {'instrument': 0, 'start': 1, 'dur': 2, 'amp': 3, 'pitch': 4}
+NOTE_SEQUENCE_NUM = 0
 
 BEATS_PER_MEASURE = 4
 BEAT_NOTE_DUR = NoteDur.QRTR
@@ -22,26 +26,36 @@ DEFAULT_IS_QUANTIZING = True
 
 @pytest.fixture()
 def note():
-    return CSoundNote(instrument=INSTRUMENT, start=START, duration=DUR, amplitude=AMP, pitch=PITCH)
+    return _note()
 
 
 @pytest.fixture
-def note_sequence(note):
-    note_1 = CSoundNote.copy(note)
-    note_2 = CSoundNote.copy(note)
-    note_2.start += DUR
-    note_3 = CSoundNote.copy(note)
-    note_3.start += (DUR * 2)
-    note_4 = CSoundNote.copy(note)
-    note_4.start += (DUR * 3)
-    note_list = [note_1, note_2, note_3, note_4]
-    return NoteSequence(note_list)
+def note_sequence():
+    return _note_sequence()
 
 
 @pytest.fixture
 def meter():
     return Meter(beat_note_dur=BEAT_NOTE_DUR, beats_per_measure=BEATS_PER_MEASURE, tempo=TEMPO_QPM,
                  quantizing=DEFAULT_IS_QUANTIZING)
+
+
+def _note():
+    # _note(): Must construct each test Note with a new instance of underlying storage to avoid aliasing bugs
+    attrs = np_copy(ATTRS)
+    return CSoundNote(attr_vals=attrs, attr_name_idx_map=ATTR_NAME_IDX_MAP, note_sequence_num=NOTE_SEQUENCE_NUM)
+
+
+def _note_sequence():
+    note_1 = _note()
+    note_2 = _note()
+    note_2.start += DUR
+    note_3 = _note()
+    note_3.start += (DUR * 2)
+    note_4 = _note()
+    note_4.start += (DUR * 3)
+    note_list = [note_1, note_2, note_3, note_4]
+    return NoteSequence(note_list)
 
 
 def test_meter():
@@ -79,19 +93,28 @@ def test_quantizing_on_off(meter):
 
 
 def _setup_test_quantize(note_sequence, meter, quantize_on=True):
-    note_list_with_longer_durations_before_quantize = [CSoundNote.copy(note) for note in note_sequence]
-    for note in note_list_with_longer_durations_before_quantize:
+    note_list_before_quantize = [_note() for _ in range(len(note_sequence))]
+    for note in note_list_before_quantize:
         note.dur = note.dur * 2
+    note_list_before_quantize[1].start += DUR
+    note_list_before_quantize[2].start += (DUR * 2)
+    note_list_before_quantize[3].start += (DUR * 3)
+
+    note_list_after_quantize = [_note() for _ in range(len(note_sequence))]
+    for note in note_list_after_quantize:
+        note.dur = note.dur * 2
+    note_list_after_quantize[1].start += DUR
+    note_list_after_quantize[2].start += (DUR * 2)
+    note_list_after_quantize[3].start += (DUR * 3)
 
     if quantize_on:
         meter.quantizing_on()
     else:
         meter.quantizing_off()
-    note_list_with_longer_durations_after_quantize = [CSoundNote.copy(note)
-                                                      for note in note_list_with_longer_durations_before_quantize]
-    meter.quantize(NoteSequence(note_list_with_longer_durations_after_quantize))
 
-    return note_list_with_longer_durations_before_quantize, note_list_with_longer_durations_after_quantize
+    meter.quantize(NoteSequence(note_list_after_quantize))
+
+    return note_list_before_quantize, note_list_after_quantize
 
 
 def test_quantize_on_off(note_sequence, meter):
@@ -126,7 +149,7 @@ def test_quantize_on_off(note_sequence, meter):
 
     # Test start adjustments
     # Expected start adjustments
-    # - First note starts at 0.0, no adjustmentj
+    # - First note starts at 0.0, no adjustment
     # - Second note is 0.25 - (note.dur * total_adjustment) = 0.125
     # - Third note is 0.5 - (note.dur * total_adjustment) = 0.375
     # - Third note is 0.75 - (note.dur * total_adjustment) = 0.625
@@ -148,19 +171,22 @@ def test_quantize_on_off(note_sequence, meter):
 
 def test_quantize_to_beat(note_sequence, meter):
     # Simplest test case: Note durations sum to measure duration and no quantizing required
-    # Also note_list is already sorted by start ascending, so the order after quantiazation will be unchanged
-    expected_note_sequence = NoteSequence.copy(note_sequence)
+    # Also note_list is already sorted by start ascending, so the order after quantization will be unchanged
+    note_starts_before_quantize = [note.start for note in note_sequence]
+    note_durations_before_quantize = [note.dur for note in note_sequence]
     meter.quantize_to_beat(note_sequence)
-    assert expected_note_sequence == note_sequence
+    note_starts_after_quantize = [note.start for note in note_sequence]
+    note_durations_after_quantize = [note.dur for note in note_sequence]
+    assert note_starts_before_quantize == note_starts_after_quantize and \
+        note_durations_before_quantize == note_durations_after_quantize
 
     # Test: Note durations not on the beat, quantization required
-    note_list_with_offset_start_times = [CSoundNote.copy(note) for note in note_sequence]
-    note_sequence_with_offset_start_times = NoteSequence(note_list_with_offset_start_times)
+    note_sequence_with_offset_start_times = _note_sequence()
     # Modify the note start_times in the copy to be offset from the beats
-    for i, note in enumerate(note_list_with_offset_start_times):
-        note.start = note.start + 0.05
+    for i, note in enumerate(note_sequence_with_offset_start_times):
+        note.start += 0.05
         assert note.start != note_sequence[i].start
-    # Quantize and assert the start times match the original start_times, which are on the beat
+    # Quantize and then assert the start times match the original start_times, which are on the beat
     meter.quantize_to_beat(note_sequence_with_offset_start_times)
     assert [note.start for note in note_sequence_with_offset_start_times] == \
            [note.start for note in note_sequence]
