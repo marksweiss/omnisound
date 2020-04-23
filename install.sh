6576e4dc19d5a8d714f3916187d8bb39b3ceed49
Mark S. Weiss <marksimonweiss@gmail.com>
2020-04-23 09:51:42 -0400

    Fixes to install

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ git clone https://github.com/marksweiss/omnisound.git
 git submodule update --init --recursive
 env_dir=$(cwd)
 python3 -m venv --prompt=omnisound "$env_dir"
-pip install --upgrade pip
 source bin/activate
+pip3 install --upgrade pip
+pip3 install -r requirement.txt
