c3fa561cd0f090309da27bb1a934a9181e812f5a
marksweiss <marksimonweiss@gmail.com>
2020-05-10 15:05:21 -0400

    Coded midi_sequence_example. Not tested yet.

--- omnisound/test/midi_sequencer_example.py
+++ omnisound/test/midi_sequencer_example.py
@@ -1,10 +1,8 @@
 # Copyright 2020 Mark S. Weiss
 
+from omnisound.note.adapters.midi_note import MidiInstrument
 from omnisound.note.containers.measure import Meter
-from omnisound.note.generators.chord_globals import HarmonicChord
 from omnisound.note.generators.midi_sequencer import MidiSequencer
-from omnisound.note.generators.scale import Scale
-from omnisound.note.generators.scale_globals import HarmonicScale, MajorKey
 from omnisound.note.modifiers.meter import NoteDur
 from omnisound.note.modifiers.swing import Swing
 
@@ -24,18 +22,41 @@ SEQUENCER_NAME = 'test_midi_sequencer_song'
 NUM_MEASURES = 4
 MIDI_FILE_PATH = '/Users/markweiss/Documents/projects/omnisound/omnisound/test/test_sequencer_song.mid'
 SEQUENCER = MidiSequencer(name=SEQUENCER_NAME, num_measures=NUM_MEASURES,
-                          pattern_resolution=BEAT_DUR, meter=METER, swing=SWING,
-                          midi_file_path=MIDI_FILE_PATH)
-# Scale
-KEY = MajorKey.C
-OCTAVE = 4
-HARMONIC_SCALE = HarmonicScale.Major
-HARMONIC_CHORD = HarmonicChord.MajorTriad
-NUM_NOTES_IN_CHORD = 3
-SCALE = Scale(key=KEY, octave=OCTAVE, harmonic_scale=HARMONIC_SCALE,
-              mn=SEQUENCER.mn)
-NUM_NOTES_IN_SCALE = 7
+                          meter=METER, swing=SWING, midi_file_path=MIDI_FILE_PATH)
 # Algo Comp Settings
 BASE_VELOCITY = 100
 VELOCITY_FACTOR = 2
 
+
+if __name__ == '__main__':
+
+    def get_velocity(j, notes_per_measure):
+        return int(BASE_VELOCITY - ((j % notes_per_measure) / VELOCITY_FACTOR))
+
+    track_name = 'ostinato'
+    instrument = MidiInstrument.Vibraphone.value
+    notes_per_measure = int(1 / NoteDur.THIRTYSECOND.value)
+    pattern_phrases = []
+    for j in range(notes_per_measure, step=4):
+        pattern_phrase = (f'C:4::{get_velocity (j, notes_per_measure)} '
+                          f'E:4::{get_velocity (j + 1, notes_per_measure)} '
+                          f'G:4::{get_velocity (j + 2, notes_per_measure)} '
+                          f'E:4::{get_velocity (j + 3, notes_per_measure)}')
+        pattern_phrases.append(pattern_phrase)
+    pattern = ' '.join(pattern_phrases)
+    SEQUENCER.add_pattern_as_new_track(track_name=track_name, pattern=pattern, instrument=instrument)
+    SEQUENCER.track(track_name).apply_swing()
+
+    track_name = 'chord'
+    instrument = MidiInstrument.Acoustic_Grand_Piano.value
+    notes_per_measure = int(1 / NoteDur.WHOLE.value)
+    pattern_measures = []
+    for i in range(NUM_MEASURES):
+        for j in range(notes_per_measure):
+            pattern_phrase = f'C:2:MajorTriad:{BASE_VELOCITY - 10}'
+            pattern_phrases.append(pattern_phrase)
+        pattern_measures.append(' '.join(pattern_phrases))
+    pattern = '|'.join(pattern_measures)
+    SEQUENCER.add_pattern_as_new_track(track_name=track_name, pattern=pattern, instrument=instrument)
+
+    SEQUENCER.play()
