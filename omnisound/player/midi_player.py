0fba9fed41cf62c1b4b3da8a042ac679f1058677
marksweiss <marksimonweiss@gmail.com>
2020-05-18 03:00:50 -0400

    Midi interactive and file player coded and no IDE errors. Not tested and not in final modules.

--- omnisound/player/midi_player.py
+++ omnisound/player/midi_player.py
@@ -1,17 +1,30 @@
 # Copyright 2020 Mark S. Weiss
 
 from enum import Enum
-from typing import Any, List, Optional
+from typing import Any, List, Optional, Sequence
 
 # noinspection PyProtectedMember
-from mido import Message, MidiFile, MidiTrack
+from mido import Message, MidiFile, MidiTrack, open_output
 
 from omnisound.note.adapters.midi_note import ATTR_GET_TYPE_CAST_MAP
 from omnisound.note.containers.measure import Measure
 from omnisound.note.containers.song import Song
+from omnisound.note.containers.track import MidiTrack as OmnisoundMidiTrack
 from omnisound.note.modifiers.meter import NoteDur
 from omnisound.player.player import Player
 from omnisound.utils.utils import validate_optional_path, validate_optional_type, validate_type, validate_types
+
+MIDI_TICKS_PER_QUARTER_NOTE = 960
+MIDI_QUARTER_NOTES_PER_BEAT = 4
+MIDI_BEATS_PER_MINUTE = 120
+MIDI_TICKS_PER_SECOND = int((MIDI_TICKS_PER_QUARTER_NOTE *
+                             MIDI_QUARTER_NOTES_PER_BEAT *
+                             MIDI_BEATS_PER_MINUTE)
+                            / 60)
+
+DEFAULT_BEAT_DURATION = NoteDur.QUARTER
+# PLAY_ALL = 'play_all'
+# PLAY_EACH = 'play_each'
 
 
 class MidiPlayerAppendMode(Enum):
@@ -53,7 +66,7 @@ class MidiPlayerEvent(object):
 
     def _tick(self) -> int:
         return int(self.measure.meter.get_secs_for_note_time(note_time_val=self.event_time) *
-                   MidiPlayer.MIDI_TICKS_PER_SECOND)
+                   MIDI_TICKS_PER_SECOND)
 
     @staticmethod
     def order_event_list(event_list: List['MidiPlayerEvent']):
@@ -67,48 +80,20 @@ class MidiPlayerEvent(object):
             event.tick_delta = event.tick - event_list[j - 1].tick
 
 
-class MidiPlayer(Player):
-    MIDI_TICKS_PER_QUARTER_NOTE = 960
-    MIDI_QUARTER_NOTES_PER_BEAT = 4
-    MIDI_BEATS_PER_MINUTE = 120
-    MIDI_TICKS_PER_SECOND = int((MIDI_TICKS_PER_QUARTER_NOTE *
-                                 MIDI_QUARTER_NOTES_PER_BEAT *
-                                 MIDI_BEATS_PER_MINUTE)
-                                / 60)
-
-    DEFAULT_BEAT_DURATION = NoteDur.QUARTER
-    PLAY_ALL = 'play_all'
-    PLAY_EACH = 'play_each'
-
+# TODO MAKE ABC
+class MidiPlayerBase(Player):
     def __init__(self,
                  song: Optional[Song] = None,
-                 append_mode: MidiPlayerAppendMode = None,
-                 midi_file_path: str = None):
+                 append_mode: MidiPlayerAppendMode = None):
         # MidiPlayer only can play a Song with one or more Tracks. Tracks may be bare NoteSequence collections
         # or Measures with Meter
-        validate_types(('append_mode', append_mode, MidiPlayerAppendMode))
-        validate_optional_type('song', song, Song)
-        validate_optional_path('midi_file_path', midi_file_path)
-        super(MidiPlayer, self).__init__()
+        super(MidiPlayerBase, self).__init__()
 
         self._song = song
         self.append_mode = append_mode
-        self.midi_file_path = midi_file_path
         self.midi_track_tick_relative = self.append_mode == MidiPlayerAppendMode.AppendAfterPreviousNote
-        # Type 1 - multiple synchronous tracks, all starting at the same time
-        # https://mido.readthedocs.io/en/latest/midi_files.html
-        self.midi_file = MidiFile(type=1)
         # TODO Support Midi Performance Attrs
         # song_performance_attrs = song.performance_attrs
-
-    def write_midi_file(self):
-        self.midi_file.save(self.midi_file_path)
-
-    def play(self):
-        self._play()
-
-    def play_each(self):
-        self._play()
 
     @property
     def song(self):
@@ -124,48 +109,127 @@ class MidiPlayer(Player):
         # for pa 0in performance_attrs_list:
         #    apply pa to note
 
-    def _play(self):
+    # TODO MAKE A GENERATOR SO INTERACTIVE PLAYER CAN STREAM
+    @staticmethod
+    def _get_midi_events_for_track(track: OmnisoundMidiTrack) -> Sequence[Message]:
+        messages = []
+
+        # mido channels numbered 0..15 instead of MIDI standard 1..16
+        channel = track.channel - 1
+        for measure in track.measure_list:
+            # TODO Support Midi Performance Attrs
+            # if op == PLAY_ALL
+            #     performance_attrs = measure.performance_attrs
+            # Build an ordered event list of the notes in the measure
+            # NOTE: Assumes first note start on 0.0, because the first note of every measure is 0 offset
+            #       i.e. it assumes it will occur exactly after the last note of the last measure
+            # NOTE: Need to carry over last offset from previous measure, and then this will work :-)
+            # TODO MAKE MEASURE ALWAYS FILL IN TRAILING (ALL?) RESTS SO THIS IS NOT AN ISSUE
+            event_list = []
+            for note in measure:
+                # noinspection PyTypeChecker
+                event_list.append(MidiPlayerEvent(note, measure, MidiEventType.NOTE_ON))
+                # noinspection PyTypeChecker
+                event_list.append(MidiPlayerEvent(note, measure, MidiEventType.NOTE_OFF))
+
+            MidiPlayerEvent.set_tick_deltas(event_list)
+            for event in event_list:
+                # TODO Support Midi Performance Attrs
+                # note_performance_attrs = note.performance_attrs
+                # if op == PLAY_ALL:
+                #     self._apply_performance_attrs(note, song_performance_attrs, track_performance_attrs,
+                #                                   performance_attrs, note_performance_attrs)
+                # else:
+                #     self._apply_performance_attrs(note, note_performance_attrs)
+                messages.append(Message(event.event_type.value, time=event.tick_delta,
+                                velocity=ATTR_GET_TYPE_CAST_MAP['velocity'](event.note.amplitude),
+                                note=ATTR_GET_TYPE_CAST_MAP['pitch'](event.note.pitch),
+                                channel=channel))
+
+            return messages
+
+    def _play(self) -> Sequence[MidiTrack]:
+        midi_tracks = []
+
         for track in self.song:
             # TODO Support Midi Performance Attrs
             # if op == PLAY_ALL
             #     track_performance_attrs = track.performance_attrs
-
             midi_track = MidiTrack()
             midi_track.append(Message('program_change', program=track.instrument, time=0))
+            messages = MidiPlayerBase._get_midi_events_for_track(track=track)
+            for message in messages:
+                midi_track.append(message)
+            midi_tracks.append(midi_track)
+
+        return midi_tracks
+
+    def play(self):
+        raise NotImplementedError('MidiPlayerBase.play should not be instantiated')
+
+    def play_each(self):
+        raise NotImplementedError('MidiPlayerBase.play_each should not be instantiated')
+
+    def improvise(self):
+        raise NotImplementedError('MidiPlayerBase.improvise should not be instantiated')
+
+
+class MidiWriter(MidiPlayerBase):
+    def __init__(self,
+                 song: Optional[Song] = None,
+                 append_mode: MidiPlayerAppendMode = None,
+                 midi_file_path: str = None):
+        validate_type('append_mode', append_mode, MidiPlayerAppendMode)
+        validate_optional_type('song', song, Song)
+        validate_optional_path('midi_file_path', midi_file_path)
+        super(MidiWriter, self).__init__(song=song, append_mode=append_mode)
+        self.midi_file_path = midi_file_path
+        # Type 1 - multiple synchronous tracks, all starting at the same time
+        # https://mido.readthedocs.io/en/latest/midi_files.html
+        self.midi_file = MidiFile(type=1)
+
+    def write_midi_file(self):
+        self.midi_file.save(self.midi_file_path)
+
+    def play(self):
+        midi_tracks = self._play()
+        for midi_track in midi_tracks:
             self.midi_file.tracks.append(midi_track)
 
-            # mido channels numbered 0..15 instead of MIDI standard 1..16
-            channel = track.channel - 1
-            for measure in track.measure_list:
-                # TODO Support Midi Performance Attrs
-                # if op == PLAY_ALL
-                #     performance_attrs = measure.performance_attrs
-                # Build an ordered event list of the notes in the measure
-                # NOTE: Assumes first note start on 0.0, because the first note of every measure is 0 offset
-                #       i.e. it assumes it will occur exactly after the last note of the last measure
-                # NOTE: Need to carry over last offset from previous measure, and then this will work :-)
-                # TODO MAKE MEASURE ALWAYS FILL IN TRAILING (ALL?) RESTS SO THIS IS NOT AN ISSUE
-                event_list = []
-                for note in measure:
-                    # noinspection PyTypeChecker
-                    event_list.append(MidiPlayerEvent(note, measure, MidiEventType.NOTE_ON))
-                    # noinspection PyTypeChecker
-                    event_list.append(MidiPlayerEvent(note, measure, MidiEventType.NOTE_OFF))
+    def play_each(self):
+        midi_tracks = self._play()
+        for midi_track in midi_tracks:
+            self.midi_file.tracks.append(midi_track)
 
-                MidiPlayerEvent.set_tick_deltas(event_list)
-                for event in event_list:
-                    # TODO Support Midi Performance Attrs
-                    # note_performance_attrs = note.performance_attrs
-                    # if op == PLAY_ALL:
-                    #     self._apply_performance_attrs(note, song_performance_attrs, track_performance_attrs,
-                    #                                   performance_attrs, note_performance_attrs)
-                    # else:
-                    #     self._apply_performance_attrs(note, note_performance_attrs)
-                    message = Message(event.event_type.value, time=event.tick_delta,
-                                      velocity=ATTR_GET_TYPE_CAST_MAP['velocity'](event.note.amplitude),
-                                      note=ATTR_GET_TYPE_CAST_MAP['pitch'](event.note.pitch),
-                                      channel=channel)
-                    midi_track.append(message)
+    def improvise(self):
+        raise NotImplementedError('MidiPlayer does not support improvising')
+
+
+# TODO SUPPORT MULTIPLE CHANNELS
+class MidiInteractivePlayer(MidiPlayerBase):
+    def __init__(self,
+                 song: Optional[Song] = None,
+                 append_mode: MidiPlayerAppendMode = None,
+                 port_name: str = None):
+        validate_types(('port_name', port_name, str), ('append_mode', append_mode, MidiPlayerAppendMode))
+        validate_optional_type('song', song, Song)
+        super(MidiInteractivePlayer, self).__init__(song=song, append_mode=append_mode)
+        # Second arg creates a virtual port that other listening applications can bind to
+        self.port = open_output(port_name, True)
+
+    # TODO MAKE A STREAMING PLAYER
+    def play(self):
+        for track in self.song:
+            # TODO GENERATOR
+            for message in self._get_midi_events_for_track(track=track):
+                self.port.send(message)
+
+    # TODO MAKE A STREAMING PLAYER
+    def play_each(self):
+        for track in self.song:
+            # TODO GENERATOR
+            for message in self._get_midi_events_for_track(track=track):
+                self.port.send(message)
 
     def improvise(self):
         raise NotImplementedError('MidiPlayer does not support improvising')
