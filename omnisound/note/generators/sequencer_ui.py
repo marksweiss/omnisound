cdd804097e374b90c33913baea8bba1e32d130cb
marksweiss <marksimonweiss@gmail.com>
2020-07-25 02:39:54 -0400

    Started UI with sample pysimplegui app

---
+++ omnisound/note/generators/sequencer_ui.py
@@ -0,0 +1,17 @@
+# noinspection PyPep8Naming
+import PySimpleGUI as sg
+
+layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
+
+# Create the window
+window = sg.Window("Demo", layout)
+
+# Create an event loop
+while True:
+    event, values = window.read()
+    # End program if user closes window or presses the OK button
+    if event in {"OK", sg.WIN_CLOSED}:
+        break
+
+window.close()
+
