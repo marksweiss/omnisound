a7bfea5e1b3ccffbe3b0fe30978dcc9b4d5b3b42
marksweiss <marksimonweiss@gmail.com>
2021-11-21 03:34:40 -0500

    cleaned up scale query code and made it interactive so user can provide root key and get scale notes back

--- omnisound/src/generator/scale_query.py
+++ omnisound/src/generator/scale_query.py
@@ -30,11 +30,11 @@
 
 import pprint
 import re
+from collections import defaultdict
 
 
 # The musical ALPHABET consists of seven letter from A through G
 ALPHABET = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
-
 
 # The twelve NOTES in Western music, along with their enharmonic equivalents
 NOTES = [
@@ -52,32 +52,15 @@ NOTES = [
     ['A##', 'B', 'Cb'],
 ]
 
+KEYS = [
+    'B#', 'C', 'C#', 'Db', 'D', 'D#', 'Eb', 'E', 'Fb', 'E#', 'F',
+    'F#', 'Gb', 'G', 'G#', 'Ab', 'A', 'A#', 'Bb', 'B', 'Cb',
+]
 
-def find_note_index(scale, search_note):
-    ''' Given a scale, find the index of a particular note '''
-    for i, note in enumerate (scale):
-        # Deal with situations where we have a list of enharmonic
-        # equivalents, as well as just a single note as and str.
-        if (type(note) == list and search_note in note or
-                type(note) != list and type(note) == str and search_note == note):
-            return i
-
-
-def rotate(scale, n):
-    ''' Left-rotate a scale by n positions. '''
-    return scale[n:] + scale[:n]
-
-
-def chromatic(key):
-    ''' Generate a chromatic scale in a given key. '''
-    # Figure out how much to rotate the NOTES list by and return
-    # the rotated version.
-    num_rotations = find_note_index (NOTES, key)
-    return rotate (NOTES, num_rotations)
-
+INTERVAL_TYPES = ['standard', 'major']
 
 # Interval names that specify the distance between two NOTES
-intervals = [
+INTERVALS = [
     ['P1', 'd2'],  # Perfect unison   Diminished second
     ['m2', 'A1'],  # Minor second     Augmented unison
     ['M2', 'd3'],  # Major second     Diminished third
@@ -94,7 +77,7 @@ intervals = [
 ]
 
 # Interval names based off the NOTES of the major scale
-intervals_major = [
+INTERVALS_MAJOR = [
     ['1', 'bb2'],
     ['b2', '#1'],
     ['2', 'bb3', '9'],
@@ -110,61 +93,7 @@ intervals_major = [
     ['8', '#7'],
 ]
 
-
-def find_note_by_root(notes, root):
-    '''
-    Given a list of NOTES, find it's ALPHABET. Useful for figuring out which
-    enharmonic equivalent we must use in a particular scale.
-    '''
-    for note in notes:
-        if note[0] == root:
-            return note
-
-
-def make_intervals(root):
-    labeled = {}
-    c = chromatic (root)
-    start_index = find_note_index (ALPHABET, root[0])
-    for i, interval in enumerate (intervals):
-        for interval_name in interval:
-            interval_index = int (interval_name[1]) - 1
-            note = c[i % len (c)]
-            note_root = ALPHABET[(start_index + interval_index) % len (ALPHABET)]
-            if note_root is not None:
-                labeled[interval_name] = find_note_by_root (note, note_root)
-    return labeled
-
-
-def make_intervals_major(root):
-    labeled = {}
-    c = chromatic (root)
-    start_index = find_note_index (ALPHABET, root[0])
-    for i, interval in enumerate (intervals_major):
-        for interval_name in interval:
-            interval_index = int (re.sub ('[b#]', '', interval_name)) - 1
-            note = c[i % len (c)]
-            note_root = ALPHABET[(start_index + interval_index) % len (ALPHABET)]
-            if note_root is not None:
-                labeled[interval_name] = find_note_by_root (note, note_root)
-    return labeled
-
-
-def make_formula(formula, labeled):
-    '''
-    Given a comma-separated interval formula, and a set of labeled
-    NOTES in a key, return the NOTES of the formula.
-    '''
-    return [labeled[x] for x in formula.split (',')]
-
-
-intervs = make_intervals ('C')
-# print('Major     :', ','.join(make_formula('P1,M2,M3,P4,P5,M6,M7,P8', intervs))) # Major
-# print('Minor     :', ','.join(make_formula('P1,M2,m3,P4,P5,m6,m7,P8', intervs))) # Natural Minor
-# print('Mel. Minor:', ','.join(make_formula('P1,M2,m3,P4,P5,M6,M7,P8', intervs))) # Melodic Minor
-# print('Har. Minor:', ','.join(make_formula('P1,M2,m3,P4,P5,m6,M7,P8', intervs))) # Harmonic Minor
-# print('Major     :', ','.join(make_formula('1,2,3,4,5,6,7', intervs))) # Major
-
-formulas = {
+FORMULAS = {
     # Scale formulas
     'scales': {
         # Basic chromatic scale
@@ -217,27 +146,29 @@ formulas = {
         'sus2': '1,2,5',
         'sus4': '1,4,5',
         '7sus2': '1,2,5,b7',
-        '7sus4': '1,4,5,b7', }, }
+        '7sus4': '1,4,5,b7',
+    },
+}
 
+FORMULA_TO_INTERVAL_TYPES = {
+    'chromatic': 'major',
+    # Major scale, its modes, and minor scale
+    'major': 'major',
+    'minor': 'major',
+    # Melodic minor and its modes
+    'melodic_minor': 'major',
+    # Harmonic minor and its modes
+    'harmonic_minor': 'major',
+    # Blues scales
+    'major_blues': 'major',
+    'minor_blues': 'major',
+    # Penatatonic scales
+    'pentatonic_major': 'major',
+    'pentatonic_minor': 'major',
+    'pentatonic_blues': 'major',
+}
 
-def dump(scale, separator=' '):
-    '''
-    Pretty-print the NOTES of a scale. Replaces b and # characters
-    for unicode flat and sharp symbols.
-    '''
-    return separator.join (['{:<3s}'.format (x) for x in scale]) \
-        .replace ('b', '\u266d') \
-        .replace ('#', '\u266f')
-
-
-intervs = make_intervals_major ('C')
-for key in formulas:
-    print (key)
-    for name, formula in formulas[key].items ():
-        v = make_formula (formula, intervs)
-        print ('\t', name, ':', dump (v))
-
-major_mode_rotations = {
+MAJOR_MODE_ROTATIONS = {
     'Ionian': 0,
     'Dorian': 1,
     'Phrygian': 2,
@@ -248,100 +179,162 @@ major_mode_rotations = {
 }
 
 
-def mode(scale, degree):
-    return rotate (scale, degree)
+class ScaleQuery:
+    def __init__(self):  # sourcery skip: hoist-statement-from-if
+        self.key_interval_note_maps = defaultdict(dict)
+        self.scales = defaultdict(dict)
+        self.modes = defaultdict(dict)
+
+        for key in KEYS:
+            for interval_type in INTERVAL_TYPES:
+                self.key_interval_note_maps[key][interval_type] = self.make_interval_note_map(key, interval_type)
+
+                for scale_type, formula in FORMULAS['scales'].items():
+                    interval_maker = self.make_intervals_major \
+                        if FORMULA_TO_INTERVAL_TYPES[scale_type] == 'major' else self.make_intervals
+                    self.scales[key][scale_type] = self.make_formula(
+                        formula,
+                        interval_maker(key)
+                    )
+
+                    for mode_name, degree in MAJOR_MODE_ROTATIONS.items():
+                        scale = self.scales[key][scale_type]
+                        rotated_scale = self.mode(scale, degree)
+                        self.modes[rotated_scale[0]][mode_name] = rotated_scale
+
+    @staticmethod
+    def find_note_index(scale, search_note):
+        """ Given a scale, find the index of a particular note """
+        for i, note in enumerate(scale):
+            # Deal with situations where we have a list of enharmonic
+            # equivalents, as well as just a single note as and str.
+            if (type(note) == list and search_note in note or
+                    type(note) != list and type(note) == str and search_note == note):
+                return i
+
+    @staticmethod
+    def rotate(scale, n):
+        """ Left-rotate a scale by n positions. """
+        return scale[n:] + scale[:n]
+
+    @staticmethod
+    def mode(scale, degree):
+        return ScaleQuery.rotate(scale, degree)
+
+    @staticmethod
+    def chromatic(key):
+        """ Generate a chromatic scale in a given key. """
+        # Figure out how much to rotate the NOTES list by and return
+        # the rotated version.
+        num_rotations = ScaleQuery.find_note_index(NOTES, key)
+        return ScaleQuery.rotate(NOTES, num_rotations)
+
+    @staticmethod
+    def make_formula(formula, labeled):
+        """
+        Given a comma-separated interval formula, and a set of labeled
+        NOTES in a key, return the NOTES of the formula.
+        """
+        return [labeled[x] for x in formula.split(',')]
+
+    @staticmethod
+    def find_note_by_root(notes, root):
+        """
+        Given a list of NOTES, find it's ALPHABET. Useful for figuring out which
+        enharmonic equivalent we must use in a particular scale.
+        """
+        for note in notes:
+            if note[0] == root:
+                return note
+
+    @staticmethod
+    def make_intervals(root):
+        labeled = {}
+        c = ScaleQuery.chromatic(root)
+        start_index = ScaleQuery.find_note_index(ALPHABET, root[0])
+        for i, interval in enumerate(INTERVALS):
+            for interval_name in interval:
+                interval_index = int(interval_name[1]) - 1
+                note = c[i % len (c)]
+                note_root = ALPHABET[(start_index + interval_index) % len(ALPHABET)]
+                if note_root is not None:
+                    labeled[interval_name] = ScaleQuery.find_note_by_root(note, note_root)
+        return labeled
+
+    @staticmethod
+    def make_intervals_major(root):
+        labeled = {}
+        c = ScaleQuery.chromatic(root)
+        start_index = ScaleQuery.find_note_index(ALPHABET, root[0])
+        for i, interval in enumerate(INTERVALS_MAJOR):
+            for interval_name in interval:
+                interval_index = int(re.sub ('[b#]', '', interval_name)) - 1
+                note = c[i % len(c)]
+                note_root = ALPHABET[(start_index + interval_index) % len(ALPHABET)]
+                if note_root is not None:
+                    labeled[interval_name] = ScaleQuery.find_note_by_root (note, note_root)
+        return labeled
+
+    def make_interval_note_map(self, key, interval_type='standard'):
+        # Our labeled set of NOTES mapping interval names to NOTES
+        labels = {}
+
+        # Step 1: Generate a chromatic scale in our desired key
+        chromatic_scale = self.chromatic(key)
+
+        # The alphabets starting at provided key
+        alphabet_key = self.rotate(ALPHABET, self.find_note_index(ALPHABET, key[0]))
+
+        intervs = INTERVALS if interval_type == 'standard' else INTERVALS_MAJOR
+        # Iterate through all intervals (list of lists)
+        for index, interval_list in enumerate(intervs):
+
+            # Step 2: Find the NOTES to search through based on degree
+            notes_to_search = chromatic_scale[index % len(chromatic_scale)]
+
+            degree = None
+            for interval_name in interval_list:
+                # Get the interval degree
+                if interval_type == 'standard':
+                    degree = int(interval_name[1]) - 1  # e.g. M3 --> 2, m7 --> 6
+                elif interval_type == 'major':
+                    degree = int(re.sub('[b#]', '', interval_name)) - 1
+
+                # Get the ALPHABET to look for
+                alphabet_to_search = alphabet_key[degree % len(alphabet_key)]
+
+                # print('Interval {}, degree {}: looking for ALPHABET {} in NOTES {}'.format(interval_name, degree,
+                #                                                                            alphabet_to_search,
+                #                                                                            notes_to_search))
+                # noinspection PyBroadException
+                try:
+                    note = [x for x in notes_to_search if x[0] == alphabet_to_search][0]
+                except:
+                    note = notes_to_search[0]
+
+                labels[interval_name] = note
+
+        return labels
+
+    @staticmethod
+    def dump(scale, separator=' '):
+        """
+        Pretty-print the NOTES of a scale. Replaces b and # characters
+        for unicode flat and sharp symbols.
+        """
+        return separator.join(['{:<3s}'.format(x) for x in scale]) \
+            .replace('b', '\u266d') \
+            .replace('#', '\u266f')
 
 
-# intervs = make_intervals_major('C')
-# v = make_formula(formulas['scales']['major_I'], intervs)
-# print(dump(mode(v, major_mode_rotations['Phrygian'])))
-#
-# print(find_note_index(NOTES, 'A'))
-# print(find_note_index(ALPHABET, 'A'))
-
-def make_intervals(key, interval_type='standard'):
-    # Our labeled set of NOTES mapping interval names to NOTES
-    labels = {}
-
-    # Step 1: Generate a chromatic scale in our desired key
-    chromatic_scale = chromatic (key)
-
-    # The alphabets starting at provided key
-    alphabet_key = rotate (ALPHABET, find_note_index (ALPHABET, key[0]))
-
-    intervs = intervals if interval_type == 'standard' else intervals_major
-    # Iterate through all intervals (list of lists)
-    for index, interval_list in enumerate (intervs):
-
-        # Step 2: Find the NOTES to search through based on degree
-        notes_to_search = chromatic_scale[index % len (chromatic_scale)]
-
-        degree = None
-        for interval_name in interval_list:
-            # Get the interval degree
-            if interval_type == 'standard':
-                degree = int (interval_name[1]) - 1  # e.g. M3 --> 2, m7 --> 6
-            elif interval_type == 'major':
-                degree = int (re.sub ('[b#]', '', interval_name)) - 1
-
-            # Get the ALPHABET to look for
-            alphabet_to_search = alphabet_key[degree % len (alphabet_key)]
-
-            print ('Interval {}, degree {}: looking for ALPHABET {} in NOTES {}'.format (interval_name, degree,
-                                                                                         alphabet_to_search,
-                                                                                         notes_to_search))
-            try:
-                note = [x for x in notes_to_search if x[0] == alphabet_to_search][0]
-            except:
-                note = notes_to_search[0]
-
-            labels[interval_name] = note
-
-    return labels
-
-
-intervs = make_intervals ('B#', 'major')
-
-# pprint.pprint(make_intervals_standard('C'), sort_dicts=False)
-#
-# formula = 'P1,M2,M3,P4,P5,M6,M7,P8'
-# for key in ALPHABET:
-#    print(key, make_formula(formula, make_intervals_standard(key)))
-#
-# for key in ALPHABET:
-#    scale = make_formula(formula, make_intervals_standard(key))
-#    print('{}: {}'.format(key, dump(scale)))
-#
-# intervs = make_intervals('C', 'major')
-# for ftype in formulas:
-#     print(ftype)
-#     for name, formula in formulas[ftype].items():
-#         v = make_formula(formula, intervs)
-#         print('\t{}: {}'.format(name, dump(v)))
-#
-# print('\n\n')
-# intervs = make_intervals('C', 'major')
-# c_major_scale = make_formula(formulas['scales']['major'], intervs)
-# for m in major_mode_rotations:
-#     v = mode(c_major_scale, major_mode_rotations[m])
-#     print('{} {}: {}'.format(dump([v[0]]), m, dump(v)))
-
-
-keys = [
-    'B#', 'C', 'C#', 'Db', 'D', 'D#', 'Eb', 'E', 'Fb', 'E#', 'F',
-    'F#', 'Gb', 'G', 'G#', 'Ab', 'A', 'A#', 'Bb', 'B', 'Cb',
-]
-
-modes = {}
-
-for key in keys:
-    print (key)
-    intervs = make_intervals (key, 'major')
-    c_major_scale = make_formula (formulas['scales']['major'], intervs)
-    for m in major_mode_rotations:
-        v = mode (c_major_scale, major_mode_rotations[m])
-        if v[0] not in modes:
-            modes[v[0]] = {}
-        modes[v[0]][m] = v
-
-pprint.pprint (modes['C'])
+if __name__ == '__main__':
+    # load data for all intervals, scales and modes
+    scale_query = ScaleQuery()
+    # block on user input and process queries for key and either scale or mode
+    while True:
+        key = input("key: ")
+        query_type = input("scale ('s') or mode ('m'): ")
+        if query_type == 's':
+            print(scale_query.scales[key.strip().upper()])
+        elif query_type == 'm':
+            print(scale_query.modes[key.strip().upper()])
